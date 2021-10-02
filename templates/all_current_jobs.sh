16c6542158007adfeaa14a852ea1df557f9b9438
hao <hao.lee0019@gmail.com>
2021-10-02 12:51:31 +0100

    initial model

---
+++ templates/all_current_jobs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash --login
+
+#$ -pe smp.pe 32
+#$ -cwd
+
+#$ -m bea
+#$ -M hao.lee0019@yahoo.com
+
+#$ -t 1-6
+mkdir ./SIM_RESULTS
+readarray -t JOB_DIRS < <(find . -mindepth 1 -maxdepth 1 -name '*Re*' -printf '%P\n')
+
+module load apps/gcc/openfoam/v1906
+module load apps/binapps/paraview/5.7.0
+source $foamDotFile
+
+. ${WM_PROJECT_DIR:?}/bin/tools/RunFunctions
+
+TID=$[SGE_TASK_ID-1]
+JOBDIR=${JOB_DIRS[$TID]}
+
+cd $JOBDIR
+echo "Running SGE_TASK_ID $SGE_TASK_ID in directory $JOBDIR"
+sh run_all.sh
+cd ..
+cp $JOBDIR/backGround/postProcessing/forceCoeffs_object/0/coefficient.dat ./SIM_RESULTS/$JOBDIR
